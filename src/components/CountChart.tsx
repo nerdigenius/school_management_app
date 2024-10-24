e523327c988c47435eb905ad3343be2ab2672e6f
nerdigenius <Ashiq.racer@gmail.com>
2024-10-24 15:53:30 +0600

    baarchart added

---
+++ src/components/CountChart.tsx
@@ -0,0 +1,66 @@
+"use client"
+import Image from "next/image";
+import { RadialBarChart, RadialBar, Legend, ResponsiveContainer } from 'recharts';
+
+const data = [
+    {
+        name: 'Total',
+        count:100,
+        fill: 'white',
+    },
+    {
+        name: 'Girls',
+        count:50,
+        fill: "#FDE047",
+    },
+    {
+        name: 'Boys',
+        count:50,
+        fill: '#7DD3FC',
+    }
+];
+
+
+
+const CountChart = () => {
+    return (
+        <div className="bg-white rounded-lg w-full h-full p-4">
+            <div className="flex justify-between items-center">
+                <h1 className="text-lg font-semibold">Students</h1>
+                <Image src={"/moreDark.png"} alt="" width={20} height={20} />
+            </div>
+            <div className="relative w-full h-[75%]">
+                <ResponsiveContainer >
+                    <RadialBarChart cx="50%" cy="50%" innerRadius="40%" outerRadius="100%" barSize={32} data={data}>
+                        <RadialBar
+                            
+                            
+                            background
+                            
+                            dataKey="count"
+                        />
+                        
+                    </RadialBarChart>
+                </ResponsiveContainer>
+                <Image src={"/maleFemale.png"}alt="" width={50} height={50} className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"/>
+            </div>
+            {/*Bottom*/}
+            <div className=" flex justify-center gap-16">
+                <div className="flex flex-col gap-1">
+                    <div className="w-5 h-5 bg-sky-300 rounded-full"/>
+                        <h1 className="font-bold">1,234</h1>
+                        <h2 className="text-sm text-gray-300">Boys (55%)</h2>
+                    
+                </div>
+                <div className="flex flex-col gap-1">
+                    <div className="w-5 h-5 bg-yellow-300 rounded-full"/>
+                        <h1 className="font-bold">1,234</h1>
+                        <h2 className="text-sm text-gray-300">Girls (55%)</h2>
+                    
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default CountChart;
--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -1,7 +1,15 @@
+import Image from "next/image";
+
 const UserCard = ({type}:{type:string}) => {
     return (
-        <div>
-            Enter
+        <div className="rounded-2xl odd:bg-purple-300 even:bg-yellow-300 p-4 flex-1 min-w-32">
+            <div className="flex justify-between items-center">
+                <span className="text-sm bg-white px-2 py-1 rounded-full text-green-600">2024/25</span>
+                <Image src={"/more.png"} alt="" width={20} height={20}/>
+                
+            </div>
+            <h1 className="text-2xl font-semibold my-4">1234</h1>
+            <h2 className="capitalize text-sm font-medium text-gray-500">{type}s</h2>
         </div>
     );
 }
